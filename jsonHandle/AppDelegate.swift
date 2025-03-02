9df6860614314dc67c443e6661509d65e0f9434e
jojo-jie <2725445401@qq.com>
2025-03-02 14:44:57 +0800

    Initial Commit

---
+++ jsonHandle/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  jsonHandle
+//
+//  Created by Walker on 2025/3/2.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
---
+++ jsonHandleTests/jsonHandleTests.swift
@@ -0,0 +1,17 @@
+//
+//  jsonHandleTests.swift
+//  jsonHandleTests
+//
+//  Created by Walker on 2025/3/2.
+//
+
+import Testing
+@testable import jsonHandle
+
+struct jsonHandleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
